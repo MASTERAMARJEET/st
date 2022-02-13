84325abf6de1aab9819df0177714766bb7359951
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-02-13 13:42:25 +0530

    change st color theme

--- config.h
+++ config.h
@@ -96,38 +96,40 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
     /* 8 normal colors */
-    "black",
-    "red3",
-    "green3",
-    "yellow3",
-    "blue2",
-    "magenta3",
-    "cyan3",
-    "gray90",
+    "#222222",
+    "#ff000f",
+    "#8ce00a",
+    "#ffb900",
+    "#008df8",
+    "#6c43a5",
+    "#00d7eb",
+    "#ffffff",
 
     /* 8 bright colors */
-    "gray50",
-    "red",
-    "green",
-    "yellow",
-    "#5c5cff",
-    "magenta",
-    "cyan",
-    "white",
+    "#444444",
+    "#ff273f",
+    "#abe05a",
+    "#ffd141",
+    "#0092ff",
+    "#9a5feb",
+    "#67ffef",
+    "#ffffff",
 
     [255] = 0,
 
     /* more colors can be added after 255 to use with DefaultXX */
-    "#cccccc",
+    "#b294bb",
     "#555555",
+    "#0d0f18",
+    "#fffaf3",
 };
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
+unsigned int defaultfg = 259;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
